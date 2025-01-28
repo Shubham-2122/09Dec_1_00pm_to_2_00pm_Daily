5b17a428aaaf474527c2a4d8cd8828de5a497025
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-01-28 14:16:40 +0530

    javscript

---
+++ Javascript/ex.js
@@ -0,0 +1,16 @@
+console.log("Hello this window JS");
+
+document.write("Hello External javscript");
+
+console.log(document.getElementById("h2"));
+
+document.getElementById("h2").innerText ="Hello this dom chnage"
+
+
+// Bom Event
+
+
+// alert("Pls Field Required form data")
+// confirm("Are your sure ?")
+// prompt("5 + 5 = ?")
+// print()
